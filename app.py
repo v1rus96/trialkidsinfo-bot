26e4f1f96752f0b827577a65c77f9137595b6a63
Firuz Akhmadov <firuz.akhmadov@gmail.com>
2019-09-06 10:10:00 +0800

    PIL

--- app.py
+++ app.py
@@ -16,8 +16,6 @@ app = Flask(__name__)
 @app.route('/{}'.format(TOKEN), methods=['POST'])
 
 def respond():
-    updates = bot.get_updates()
-    print([u.message.photo for u in updates if u.message.photo])
     # retrieve the message in JSON and then transform it to Telegram object
     update = telegram.Update.de_json(request.get_json(force=True), bot)
 
