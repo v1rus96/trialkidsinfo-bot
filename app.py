ddd7eff6041ba8f67bb67946624126796f19322f
Firuz Akhmadov <firuz.akhmadov@gmail.com>
2019-09-10 12:47:59 +0800

    Testing telebot 5

--- app.py
+++ app.py
@@ -19,27 +19,27 @@ bot = telebot.TeleBot(token=TOKEN)
 app = Flask(__name__)
 
 @app.route('/{}'.format(TOKEN), methods=['POST'])
-def respond():
-    # retrieve the message in JSON and then transform it to Telegram object
-    update = telegram.Update.de_json(request.get_json(force=True), bot)
-    print (update)
-    chat_id = update.message.chat.id
-    msg_id = update.message.message_id
+# def respond():
+#     # retrieve the message in JSON and then transform it to Telegram object
+#     update = telegram.Update.de_json(request.get_json(force=True), bot)
+#     print (update)
+#     chat_id = update.message.chat.id
+#     msg_id = update.message.message_id
 
-    # Telegram understands UTF-8, so encode text for unicode compatibility
-    text = update.message.text.encode('utf-8').decode()
-    print("got text message :", text)
+#     # Telegram understands UTF-8, so encode text for unicode compatibility
+#     text = update.message.text.encode('utf-8').decode()
+#     print("got text message :", text)
 
-    response = get_response(text)
-    bot.send_message(chat_id=chat_id, text=response, reply_to_message_id=msg_id)
-    bio = generateImage(kID=text)
-    keyboard = InlineKeyboardMarkup(inline_keyboard=[
-                   [InlineKeyboardButton(text='Press me', callback_data='press')],
-                   [InlineKeyboardButton(text='Press me', callback_data='press')],
-               ])
-    bot.send_photo(chat_id, photo=bio, reply_markup=keyboard)
+#     response = get_response(text)
+#     bot.send_message(chat_id=chat_id, text=response, reply_to_message_id=msg_id)
+#     bio = generateImage(kID=text)
+#     keyboard = InlineKeyboardMarkup(inline_keyboard=[
+#                    [InlineKeyboardButton(text='Press me', callback_data='press')],
+#                    [InlineKeyboardButton(text='Press me', callback_data='press')],
+#                ])
+#     bot.send_photo(chat_id, photo=bio, reply_markup=keyboard)
 
-    return 'ok'
+#     return 'ok'
 
 @bot.message_handler(content_types=["text"])
 def any_msg(message):
