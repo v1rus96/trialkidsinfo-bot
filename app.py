0fa96c0d273350464e7857dd63f51aeb7c18c294
Firuz Akhmadov <firuz.akhmadov@gmail.com>
2019-09-10 12:34:10 +0800

    Testing telebot

--- app.py
+++ app.py
@@ -1,21 +1,25 @@
 import telegram
+import telebot
+import pyTelegramBotAPI
 from PIL import ImageFont
 from PIL import ImageDraw
 from PIL import Image
 from io import BytesIO
 from flask import Flask, request
 from telegram import InlineKeyboardMarkup, InlineKeyboardButton
-from telebot.credentials import bot_token, bot_user_name,URL
-from telebot.mastermind import get_response
+from telegram.ext import Updater, CommandHandler, CallbackQueryHandler, ConversationHandler
+from telebots.credentials import bot_token, bot_user_name,URL
+from telebots.mastermind import get_response
+import re
+from telebot import types
 
 global bot
 global TOKEN
 TOKEN = bot_token
-bot = telegram.Bot(token=TOKEN)
+bot = telebot.TeleBot(token=TOKEN)
 app = Flask(__name__)
 
 @app.route('/{}'.format(TOKEN), methods=['POST'])
-
 def respond():
     # retrieve the message in JSON and then transform it to Telegram object
     update = telegram.Update.de_json(request.get_json(force=True), bot)
@@ -28,7 +32,7 @@ def respond():
     print("got text message :", text)
 
     response = get_response(text)
-    bot.sendMessage(chat_id=chat_id, text=response, reply_to_message_id=msg_id)
+    bot.send_message(chat_id=chat_id, text=response, reply_to_message_id=msg_id)
     bio = generateImage(kID=text)
     keyboard = InlineKeyboardMarkup(inline_keyboard=[
                    [InlineKeyboardButton(text='Press me', callback_data='press')],
@@ -38,6 +42,47 @@ def respond():
 
     return 'ok'
 
+@bot.message_handler(content_types=["text"])
+def any_msg(message):
+    keyboardmain = types.InlineKeyboardMarkup(row_width=2)
+    first_button = types.InlineKeyboardButton(text="1button", callback_data="first")
+    second_button = types.InlineKeyboardButton(text="2button", callback_data="second")
+    keyboardmain.add(first_button, second_button)
+    bot.send_message(message.chat.id, "testing kb", reply_markup=keyboardmain)
+
+@bot.callback_query_handler(func=lambda call:True)
+def callback_inline(call):
+    if call.data == "mainmenu":
+
+        keyboardmain = types.InlineKeyboardMarkup(row_width=2)
+        first_button = types.InlineKeyboardButton(text="1button", callback_data="first")
+        second_button = types.InlineKeyboardButton(text="2button", callback_data="second")
+        keyboardmain.add(first_button, second_button)
+        bot.edit_message_text(chat_id=call.message.chat.id,message_id=call.message.message_id, text="menu",reply_markup=keyboardmain)
+
+    if call.data == "first":
+        keyboard = types.InlineKeyboardMarkup()
+        rele1 = types.InlineKeyboardButton(text="1t", callback_data="1")
+        rele2 = types.InlineKeyboardButton(text="2t", callback_data="2")
+        rele3 = types.InlineKeyboardButton(text="3t", callback_data="3")
+        backbutton = types.InlineKeyboardButton(text="back", callback_data="mainmenu")
+        keyboard.add(rele1, rele2, rele3, backbutton)
+        bot.edit_message_text(chat_id=call.message.chat.id,message_id=call.message.message_id, text="replaced text",reply_markup=keyboard)
+
+    elif call.data == "second":
+        keyboard = types.InlineKeyboardMarkup()
+        rele1 = types.InlineKeyboardButton(text="another layer", callback_data="gg")
+        backbutton = types.InlineKeyboardButton(text="back", callback_data="mainmenu")
+        keyboard.add(rele1,backbutton)
+        bot.edit_message_text(chat_id=call.message.chat.id,message_id=call.message.message_id, text="replaced text",reply_markup=keyboard)
+
+    elif call.data == "1" or call.data == "2" or call.data == "3":
+        bot.answer_callback_query(callback_query_id=call.id, show_alert=True, text="alert")
+        keyboard3 = types.InlineKeyboardMarkup()
+        button = types.InlineKeyboardButton(text="lastlayer", callback_data="ll")
+        keyboard3.add(button)
+        bot.edit_message_text(chat_id=call.message.chat.id,message_id=call.message.message_id, text="last layer",reply_markup=keyboard3)
+
 def generateImage(kID):
     img = Image.new("RGB", (500,550), color="red")
     #x,y = img.size
@@ -45,7 +90,7 @@ def generateImage(kID):
     img.paste(Image.open("images/background.png"))
     draw = ImageDraw.Draw(img)
     fnt = ImageFont.truetype('images/Quicksand-Bold.ttf', 48)
-    width, height = draw.textsize(kID, fnt)
+    width = draw.textsize(kID, fnt)
     draw.text(((500-width)/2,35),kID,(255,255,255),font=fnt)
     #img.save('final.png')
     bio = BytesIO()
@@ -56,7 +101,7 @@ def generateImage(kID):
 
 @app.route('/set_webhook', methods=['GET', 'POST'])
 def set_webhook():
-    s = bot.setWebhook('{URL}{HOOK}'.format(URL=URL, HOOK=TOKEN))
+    s = bot.set_webhook('{URL}{HOOK}'.format(URL=URL, HOOK=TOKEN))
     if s:
         return "webhook setup ok"
     else:
--- telebot/credentials.py
+++
@@ -1,3 +0,0 @@
-bot_token = "880055204:AAGeIliCzZvmW6mxtUlT1N799tpwu4znpf8"
-bot_user_name = "IntroKidsBot"
-URL = "https://trialkidsbot.herokuapp.com/"
