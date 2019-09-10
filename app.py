91aeb4f11f2cc525d073535bf5093fab3863ab04
Firuz Akhmadov <firuz.akhmadov@gmail.com>
2019-09-10 12:38:44 +0800

    Testing telebot 2

--- app.py
+++ app.py
@@ -8,8 +8,8 @@ from io import BytesIO
 from flask import Flask, request
 from telegram import InlineKeyboardMarkup, InlineKeyboardButton
 from telegram.ext import Updater, CommandHandler, CallbackQueryHandler, ConversationHandler
-from telebot.credentials import bot_token, bot_user_name,URL
-from telebot.mastermind import get_response
+from telebotic.credentials import bot_token, bot_user_name,URL
+from telebotic.mastermind import get_response
 import re
 from telebot import types
 
