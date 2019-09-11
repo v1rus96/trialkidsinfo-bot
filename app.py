430e4082f15bef658fb6ec63dfbff367d054fa9a
Firuz Akhmadov <firuz.akhmadov@gmail.com>
2019-09-11 17:15:06 +0800

    TEst7

--- app.py
+++ app.py
@@ -23,7 +23,7 @@ def respond():
     # retrieve the message in JSON and then transform it to Telegram object
     update = telegram.Update.de_json(request.get_json(force=True), bot)
     print (update)
-    chat_id = update.message.chat.id
+    chat_id = update.message.chat_id
     msg_id = update.message.message_id
 
     # Telegram understands UTF-8, so encode text for unicode compatibility
