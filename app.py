ba811a5d4bc65f8a1782d0b8b4b83d106d114ea1
Firuz Akhmadov <firuz.akhmadov@gmail.com>
2019-09-12 19:35:47 +0800

    Works v4

--- app.py
+++ app.py
@@ -104,7 +104,7 @@ def process_sex_step(message):
 # Enable saving next step handlers to file "./.handlers-saves/step.save".
 # Delay=2 means that after any change in next step handlers (e.g. calling register_next_step_handler())
 # saving will hapen after delay 2 seconds.
-bot.enable_save_next_step_handlers(delay=2)
+bot.enable_save_next_step_handlers(delay=1)
 
 # Load next_step_handlers from save file (default "./.handlers-saves/step.save")
 # WARNING It will work only if enable_save_next_step_handlers was called!
