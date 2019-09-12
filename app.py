434250918abb17edea121fd71224e7c07ad9d58f
Firuz Akhmadov <firuz.akhmadov@gmail.com>
2019-09-12 19:14:31 +0800

    Works v3

--- app.py
+++ app.py
@@ -36,6 +36,15 @@ def generateImage(kID):
     bio.seek(0)
     return bio
 
+@bot.message_handler(commands=['start'])
+def start(message):
+  sent = bot.send_message(message.chat.id, 'Please describe your problem.')
+  bot.register_next_step_handler(sent, hello)
+
+def hello(message):
+    open('problem.txt', 'w').write(message.chat.id + ' | ' + message.text + '||')
+    bot.send_message(message.chat.id, 'Thank you!')
+
 @bot.message_handler(content_types=["text"])
 def echo(m):
     if m.text == 'Add kid':
