b4f34b1993ab1656080013cd861a48e5ac6b07cb
Firuz Akhmadov <firuz.akhmadov@gmail.com>
2019-09-17 15:48:25 +0800

    data5

--- app.py
+++ app.py
@@ -66,8 +66,13 @@ def echo(m):
         # keyboardmain.add(first_button, second_button)
         # bot.send_photo(m.chat.id, photo=generateImage(kID=m.text))
         find = MessageModel.get_one(args={'name': '364884'}, filters={'_id': 0})
-
-        print (find)
+        if find:
+            for c in find:
+                c_name = find['name']
+                c_age = find['age']
+                c_sex = find['sex']
+                ct = u'Name: {name}\nAge: {age}\nSex: {sex}'.format(name=c_name, age=c_age, sex=c_sex)
+        print (ct)
         msg = bot.reply_to(m, "What is kids ID?")
         return bot.register_next_step_handler(msg, process_name_step)#, reply_markup=keyboardmain)
     # else:
