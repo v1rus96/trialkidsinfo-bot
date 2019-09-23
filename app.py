fb72a92cfa2d2f8ea09cf835acace1aae1d7968a
Firuz Akhmadov <firuz.akhmadov@gmail.com>
2019-09-24 01:39:58 +0800

    done prompt

--- app.py
+++ app.py
@@ -139,20 +139,6 @@ def process_id_step(message):
         markups.add('Art', 'Science')
         msg = bot.send_message(chat_id, 'What kids like?', reply_markup=markups)
         return bot.register_next_step_handler(msg, process_brain_step)
-        # keyboardmain = types.InlineKeyboardMarkup(row_width=3)
-        # first_button = types.InlineKeyboardButton(text="Button", switch_inline_query_current_chat="Check")
-        # second_button = types.InlineKeyboardButton(text="Button", callback_data="second")
-        # third_button = types.InlineKeyboardButton(text="Button", callback_data="third")
-        # keyboardmain.add(first_button, second_button,third_button)
-        # sent = bot.send_photo(chat_id=-1001341610441, photo=generateImage(kID=user.name), reply_markup=keyboardmain)
-        # bot.send_message(chat_id, 'Nice to meet you ' + user.name + '\n Age:' + str(user.age) + '\n Sex:' + user.sex,reply_markup=keyboard())
-        # MessageModel.save_one({
-        #     'chat_id': -1001341610441,
-        #     'message_id': sent.message_id,
-        #     'name': user.name,
-        #     'age': user.age,
-        #     'sex': user.sex
-        # })
     except Exception as e:
         bot.reply_to(message, 'oooops' + e)
 
@@ -181,14 +167,52 @@ def process_game_step(message):
         user = user_dict[chat_id]
         if (game == 'Minecraft') or (game == 'Roblox') or (game == 'Both'):
             user.game = game
-            # msg = bot.send_message(chat_id, "Experience?", reply_markup=types.ForceReply())
-            # return bot.register_next_step_handler(msg, process_experience_step)
+            msg = bot.send_message(chat_id, "Experience?", reply_markup=types.ForceReply())
+            return bot.register_next_step_handler(msg, process_experience_step)
         else:
             raise Exception()
     except Exception as e:
         bot.reply_to(message, 'oooops' + e)
 
+def process_experience_step(message):
+    try:
+        chat_id = message.chat.id
+        experience = message.text
+        user = user_dict[chat_id]
+        user.experience = experience
+        markups = types.ReplyKeyboardMarkup(one_time_keyboard=True, resize_keyboard=True)
+        markups.add('Mobile', 'Robotics')
+        msg = bot.send_message(chat_id, 'What kids like?', reply_markup=markups)
+        return bot.register_next_step_handler(msg, process_interest_step)
+    except Exception as e:
+        bot.reply_to(message, 'oooops' + e)
 
+def process_interest_step(message):
+    try:
+        chat_id = message.chat.id
+        message_id = message.message_id
+        interest = message.text
+        user = user_dict[chat_id]
+        if (interest == 'Mobile') or (interest == 'Robotics'):
+            user.interest = interest
+        else:
+            raise Exception()
+        keyboardmain = types.InlineKeyboardMarkup(row_width=3)
+        first_button = types.InlineKeyboardButton(text="Button", switch_inline_query_current_chat="Check")
+        second_button = types.InlineKeyboardButton(text="Button", callback_data="second")
+        third_button = types.InlineKeyboardButton(text="Button", callback_data="third")
+        keyboardmain.add(first_button, second_button,third_button)
+        sent = bot.send_photo(chat_id=-1001341610441, photo=generateImage(kID=user.name), reply_markup=keyboardmain)
+        bot.send_message(chat_id, 'Nice to meet you ' + user.name + '\n Age:' + str(user.age) + '\n Sex:' + user.sex,reply_markup=keyboard())
+        MessageModel.save_one({
+            'chat_id': -1001341610441,
+            'message_id': sent.message_id,
+            'name': user.name,
+            'age': user.age,
+            'sex': user.sex
+        })
+    except Exception as e:
+        bot.reply_to(message, 'oooops' + e)
 
 bot.enable_save_next_step_handlers(delay=2)
 
