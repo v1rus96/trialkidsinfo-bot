3c8a27d7f4b71d502cbb5392f4bbe2f85175df2e
Firuz Akhmadov <firuz.akhmadov@gmail.com>
2019-09-30 14:53:35 +0800

    fix

--- app.py
+++ app.py
@@ -88,8 +88,8 @@ def generateImage(kID):
 
 @bot.message_handler(content_types=["text"])
 def echo(m):
+    chat_id = m.chat.id
     if m.text == 'Add kid':
-        chat_id = m.chat.id
         # MessageModel.update_message(args={'name': '364884'}, set_query={ "$set": {'age': '66'} })
         # find = MessageModel.get_one(args={'name': '364884'}, filters={'_id': 0})
         # find2 = MessageModel.get_all(args={}, filters={'_id': 0, 'name': 1})
