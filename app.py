175346a39d343ac4d9b7635d0079b0398d7c6848
Firuz Akhmadov <firuz.akhmadov@gmail.com>
2019-10-03 15:41:33 +0800

    d

--- app.py
+++ app.py
@@ -335,7 +335,7 @@ def keyboard():
 @bot.inline_handler(lambda query: len(query.query) is 0)
 def empty_query(query):
     date = str(datetime.now().date())
-    find = MessageModel.get_all(args={'date': date, 'session': 4}, filters={'_id': 0, 'name': 1})
+    find = MessageModel.get_all(args={'date': date, 'session': 3}, filters={'_id': 0, 'name': 1})
     print(find)
     hint = "Введите ровно 2 числа и получите результат!"
     results_array = []
